b9ae28bcefd4b5ae420f6cb2caec5c80fa8d78ed
Ivan <uvanes21@yandex.ru>
2022-05-04 05:44:52 +0300

    First commit

---
+++ src/main/java/schoolhogwarts/simplehogwarts/SimpleHogwartsApplication.java
@@ -0,0 +1,13 @@
+package schoolhogwarts.simplehogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHogwartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHogwartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/schoolhogwarts/simplehogwarts/SimpleHogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package schoolhogwarts.simplehogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHogwartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
